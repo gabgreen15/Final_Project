1f4c37f31b264c67f4660952e64c17a15b2b9a8e
gabriellegreen <gabriellegreen@35.40.189.138>
2018-11-15 11:16:24 -0500

    void Initialize_Pins(void)

--- Final_Project/main.c
+++ Final_Project/main.c
@@ -3,9 +3,38 @@
 
 /**
  * Jenna Stolzman and Gabrielle Green
+ *
  */
+
+void Initialize_Pins(void);
+
 void main(void)
 {
 	WDT_A->CTL = WDT_A_CTL_PW | WDT_A_CTL_HOLD;		// stop watchdog timer
+	Initialize_Pins();
 
+}
+/*
+ * void Initialize_Pins(void)
+ *
+ * Functions to initialize pins used in alarm clock
+ */
+void Initialize_Pins(void)
+{
+    //LCD pins
+    //Initialize P6.0 E and 6.1 RS
+    P6->SEL0 &= ~(BIT0 | BIT1); //sets up P4.0 GPIO
+    P6->SEL1 &= ~(BIT0 | BIT1); //sets up P4.0 as GPIO
+    P6->DIR |= (BIT0 | BIT1); //sets up P4.0 as an output
+
+    //Initialize P5.4 DB4, P5.5 DB5, P5.6 DB6, P5.7 DB7
+    P5->SEL0 &= ~(BIT4 | BIT5 | BIT6 | BIT7); //sets as GPIO
+    P5->SEL1 &= ~(BIT4 | BIT5 | BIT6 | BIT7); //sets as GPIO
+    P5->DIR |= (BIT4 | BIT5 | BIT6 | BIT7); //sets as an output
+
+    //LED pins as PWM with TimerA
+    P2->SEL0 |= (BIT4 | BIT5); //set as Timer A
+    P2->SEL1 &= ~(BIT4 | BIT5); //set as Timer A
+    P2->DIR |= (BIT4 | BIT5); //set as output
+    P2->OUT &= ~(BIT4 | BIT5);
 }
